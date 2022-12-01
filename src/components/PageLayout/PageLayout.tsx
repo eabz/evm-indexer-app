477d99c032c3642fe68d488a0789810e018dc9f3
eabz <eabz@kindynos.mx>
2022-12-01 14:25:47 -0600

    Fixes

--- src/components/PageLayout/PageLayout.tsx
+++ src/components/PageLayout/PageLayout.tsx
@@ -1,4 +1,5 @@
-import { Text, VStack } from '@chakra-ui/layout'
+import { Box, Text, VStack } from '@chakra-ui/layout'
+import Link from 'next/link'
 
 import { SearchBar } from '../SearchBar'
 
@@ -6,14 +7,14 @@ export const PageLayout = ({ children }: { children: JSX.Element }) => {
   return (
     <VStack align="center" justifyContent="center" marginTop="2" width="full">
       <Text fontSize="2xl" fontWeight="bold" marginTop="10" textAlign="center">
-        EVM Indexer
+        <Link href="/">EVM Indexer</Link>
       </Text>
       <Text fontSize="md" fontWeight="regular" padding="2" textAlign="center">
         Minimalistic EVM compatible chain indexer
       </Text>
 
       <SearchBar />
-      {children}
+      <Box width="full">{children}</Box>
     </VStack>
   )
 }
--- src/pages/api-doc/index.tsx
+++ src/pages/api-doc/index.tsx
@@ -4,13 +4,19 @@ import { GetStaticProps, InferGetStaticPropsType } from 'next'
 import dynamic from 'next/dynamic'
 import { createSwaggerSpec } from 'next-swagger-doc'
 
+import { PageLayout } from '@/components/PageLayout'
+
 const SwaggerUI = dynamic<{
   spec: Record<string, any>
   // @ts-ignore
 }>(import('swagger-ui-react'), { ssr: false })
 
 function ApiDoc({ spec }: InferGetStaticPropsType<typeof getStaticProps>) {
-  return <SwaggerUI spec={spec} />
+  return (
+    <PageLayout>
+      <SwaggerUI spec={spec} />
+    </PageLayout>
+  )
 }
 
 export const getStaticProps: GetStaticProps = async () => {
